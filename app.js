ebd8c513a20164e2518fcc473a8977d0a3ad66f7
kingston180 <nparihar21.mp@gmail.com>
2022-08-19 21:19:05 +0530

    firstc

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.json())
 dotenv.config({path:'./config.env'})
 require("./Database/connection");
 app.use(require("./auth"))
-
-app.listen(process.env.port,()=>{
+const port=5000||process.env.port;
+app.listen(port,()=>{
     console.log("Server is running on port no. 3000...")
 })
